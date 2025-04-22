730caffc5e45b883e0d51e6ff5a5da9435353f2e
Giovana <giovana@prover.net.br>
2025-04-22 18:12:16 -0300

    pagina inicial em desenvolvimento

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,53 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar style="background-color: #282828; height: 80px; display: flex; align-items: center; text-align: center;">
+          <div style="width: 100%;">
+            <img src="../../public/Logo_CasaMantem.png" alt="" height="70px">
+          </div>
+          <div v-if="!token" style="display: flex; position: absolute; right: 0;" class="q-pr-md">
+            <q-btn color="primary" no-caps class="q-px-md" style="border-radius: 10px;" @click="''">
+              <q-icon name="fas fa-user" size="18px" />
+              <span v-if="!$q.screen.xs" class="q-pl-sm">Entrar/Cadastrar</span>
+            </q-btn>
+          </div>
+          <div v-if="pagina != 'inicial'" style="display: flex; position: absolute; left: 0;" class="q-pl-md">
+            <q-btn color="primary" no-caps class="q-px-md" style="border-radius: 10px;" @click="''">
+              <q-icon name="fas fa-angle-left" size="18px" />
+              <span class="q-pl-sm">Voltar</span>
+            </q-btn>
+          </div>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  data () {
+    return {
+      token: '',
+      pagina: 'inicial'
+    }
+  },
+  methods: {
+    recuperaToken () {
+      this.token = this.$q.sessionStorage.getItem('token')
+    },
+    recuperaPagina () {
+      this.pagina = this.$route.name
+    }
+  },
+  created () {
+    this.recuperaToken()
+  }
+})
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,121 @@
+<template>
+  <q-page padding>
+    <!-- Ilustrações -->
+    <div class="q-pt-lg flex items-center justify-evenly">
+      <div>
+        <q-img
+          src="../../public/Ilustração1.jpg"
+          no-spinner
+          width="390px"
+        />
+      </div>
+      <div v-if="!$q.screen.xs">
+        <q-img
+          src="../../public/Ilustração2.jpg"
+          no-spinner
+          width="580px"
+        />
+      </div>
+    </div>
+
+    <!-- Selects -->
+    <div class="q-pt-lg row">
+      <div :class="$q.screen.xs ? 'col-xs-12 col-sm-6 q-py-sm' : 'col-xs-12 col-sm-6 q-pr-xl q-py-sm'" :align="$q.screen.xs ? '' : 'right'">
+        <q-select :style="$q.screen.xs ? '' : 'width: 65%;'" v-model="servico" :options="opt_servicos" label="Procure o serviço que deseja..." filled />
+      </div>
+      <div :class="$q.screen.xs ? 'col-xs-12 col-sm-6 q-py-sm' : 'col-xs-12 col-sm-6 q-pl-xl q-py-sm'" :align="$q.screen.xs ? '' : 'left'">
+        <q-select :style="$q.screen.xs ? '' : 'width: 65%;'" v-model="localidade" :options="opt_localidades" label="Procure a localidade..." filled />
+      </div>
+    </div>
+
+    <!-- Serviços -->
+    <div align="center">
+      <div style="font-size: large;" class="text-bold q-py-xl">
+        Serviços que encontramos em: Santa Maria - RS <!-- Substituir por variável que armazene a localização do usuário -->
+      </div>
+
+      <div>
+        <q-card style="width: 70%; border-radius: 10px;" class="my-card">
+          <q-card-section>
+            <q-list>
+              <q-item v-for="(prestador) in lstservicos" :key="prestador.id">
+                <q-item-section avatar>
+                  <q-avatar size="64px">
+                    <img :src="`https://cdn.quasar.dev/img/${prestador.avatar}`">
+                  </q-avatar>
+                </q-item-section>
+                <q-item-section align="left">
+                  <div style="font-size: medium;" class="text-bold">
+                    {{prestador.name}}
+                  </div>
+                  <div>
+                    <span v-for="(servico, index) in prestador.servicos" :key="index">
+                      {{ servico }}{{ index + 1 === prestador.servicos.length ? '' : ', ' }}
+                    </span>
+                  </div>
+                </q-item-section>
+                <q-item-section>
+                  <div>
+                    {{ prestador.cidade }} - {{ prestador.estado }}
+                  </div>
+                  <div>
+                    <q-icon name="fas fa-star" size="12px" />
+                    <span class="q-pl-sm">{{ prestador.nota }}</span> • <span>{{prestador.nravaliacoes }} {{ prestador.nravaliacoes === 1 ? 'avaliação' : 'avaliações'}}</span>
+                  </div>
+                </q-item-section>
+                <q-item-section side>
+                  <div>
+                    <q-btn v-if="!$q.screen.xs" color="primary" label="Contratar" no-caps @click="''" />
+                  </div>
+                </q-item-section>
+              </q-item>
+            </q-list>
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage',
+
+  data () {
+    return {
+      lstservicos: [{
+        id: 5,
+        name: 'Luciana de Souza',
+        avatar: 'avatar2.jpg',
+        servicos: ['Eletricista', 'Encanador'],
+        cidade: 'Santa Maria',
+        estado: 'RS',
+        nota: 4.8,
+        nravaliacoes: 23
+      }, {
+        id: 6,
+        name: 'Gabrielle Almeida Ferreira',
+        avatar: 'avatar6.jpg',
+        servicos: ['Diarista'],
+        cidade: 'Santa Maria',
+        estado: 'RS',
+        nota: 4.9,
+        nravaliacoes: 16
+      }],
+      opt_servicos: [],
+      opt_localidades: [],
+      servico: '',
+      localidade: ''
+    }
+  }
+})
+</script>
+
+<style>
+.q-select--without-input .q-field__control {
+    cursor: pointer;
+    border-radius: 10px;
+}
+</style>
